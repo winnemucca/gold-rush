56b5b7fa50d1fac3eea8626c125986b55408ed80
Steve Kauyedauty <skauyedauty@gmail.com>
2014-06-30 15:04:03 -0600

    completed part1

--- main.js
+++ main.js
@@ -8,7 +8,15 @@ $(document).on('ready', function() {
 		var star= $('<img id="mapstar" src="http://icongal.com/gallery/download/326647/128/png" >');
 
 		$('.map').before(star);
+		// $(star).css("top", event.pageX, "left", event.pageY, "position", "absolute", "z-index", "1", "height", 30 );
+			// console.log("mapstar works");
+			$(star).css({
+				top: event.pageY-30, 
+				left: event.pageX-17, 
+				position: "absolute", 
+				zIndex: "1", 
+				height: 30
+			});
 			console.log("mapstar works");
-	
 	});
 });
