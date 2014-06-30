ff24b8199496aafee2ae19d5de2695577a80bb22
Steve Kauyedauty <skauyedauty@gmail.com>
2014-06-30 15:33:30 -0600

    started progress on 2

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 $(document).on('ready', function() {
-  
+  		//position allows user to select one icon per click
+		// var star= $('<img id="mapstar" src="http://icongal.com/gallery/download/326647/128/png" >');
 
 
 	$('.map').on('click', function(event) {
 		console.log("pageX: " + event.pageX + " pageY: " + event.pageY);
 
 		var star= $('<img id="mapstar" src="http://icongal.com/gallery/download/326647/128/png" >');
-
+		// position allows user to select multiple selections per click
 		$('.map').before(star);
 		// $(star).css("top", event.pageX, "left", event.pageY, "position", "absolute", "z-index", "1", "height", 30 );
 			// console.log("mapstar works");
@@ -16,7 +17,19 @@ $(document).on('ready', function() {
 				position: "absolute", 
 				zIndex: "1", 
 				height: 30
+
 			});
+			//part 2 attempted to reselect the image and delete it.  showed no effect
+			// $(star).prepend('<div class="removeMe"> </div>');
+			// $('.removeMe').click(function(star){
+			// $(this).remove();
+			// console.log("removed icon");
+			// });
 			console.log("mapstar works");
+
+
+
 	});
+
+
 });
